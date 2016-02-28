f690c12d8f4c9c67d7b857c82a0a580b7d87671c
nickyua <nikitabakunov01@gmail.com>
2016-02-29 01:00:06 +0200

    add comments to source code

--- solve.R
+++ solve.R
@@ -1,8 +1,12 @@
+#read data
 data <- read.table("data.txt", sep = ";", na.strings = "?", header = TRUE);
 data1 <- data;
 data1[,1] = as.Date(data1[,1], "%d/%m/%Y");
+#subseting data
 data2 <- subset(data1, Date > as.Date("31/1/2007", "%d/%m/%Y") &
                   Date < as.Date("3/2/2007", "%d/%m/%Y"))
+
+#converting data and time to right format
 data2["newtime"] <- Sys.time();
 for (i in 1:dim(data2)[1])
 {
@@ -10,19 +14,23 @@ for (i in 1:dim(data2)[1])
   newtime <- strptime(a, "%Y-%m-%d %H:%M:%S");
   data2$newtime[i] <-newtime;
 }
+
 par(mfrow = c(1, 1), mar = c(5, 5, 1, 1))
+# first hist
 hist(data2[,3], col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power");
 
+#second plot
 plot(data2[,3]~data2[,10], type = "n", xlab = "", ylab = "Global Active Power (kilowatts)");
 lines(data2[,3]~data2[,10]);
 
+#third plot
 plot(data2[, 7]~data2[,10], type = "n", xlab = "", ylab="Energy sub metering")
-
 lines(data2[, 7]~data2[,10], col = "black");
 lines(data2[, 8]~data2[,10], col = "red");
 lines(data2[, 9]~data2[,10], col = "blue");
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), col =c("black", "red", "blue"));
 
+#fourth picture(set of 4 plots)
 par(mfrow = c(2, 2), mar = c(5, 5, 1, 1))
 plot(data2[,3]~data2[,10], type = "n", xlab = "", ylab = "Global Active Power");
 lines(data2[,3]~data2[,10]);
